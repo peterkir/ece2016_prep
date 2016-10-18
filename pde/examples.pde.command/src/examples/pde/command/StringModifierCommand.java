6377f2343c22c811b1eaa5c63bd93c0ef175f61b
Dirk Fauth <dirk.fauth@googlemail.com>
2016-10-18 22:42:24 +0200

    Exercise: Configurable Service

--- pde/examples.pde.command/src/examples/pde/command/StringModifierCommand.java
+++ pde/examples.pde.command/src/examples/pde/command/StringModifierCommand.java
@@ -1,9 +1,15 @@
 package examples.pde.command;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
 
 import examples.service.api.StringModifier;
 
@@ -15,7 +21,7 @@ import examples.service.api.StringModifier;
 		service = StringModifierCommand.class)
 public class StringModifierCommand {
 
-	private StringModifier modifier;
+	private List<StringModifier> modifier = new ArrayList<>();
 
 	@Activate
 	void activate() {
@@ -27,12 +33,24 @@ public class StringModifierCommand {
 		System.out.println(this.getClass() + " deactivated");
 	}
 	
-	@Reference
+	@Reference(
+			cardinality=ReferenceCardinality.AT_LEAST_ONE,
+			policy=ReferencePolicy.DYNAMIC
+	)
 	void bindStringModifier(StringModifier modifier) {
-		this.modifier = modifier;
+		this.modifier.add(modifier);
+	}
+	
+	void unbindStringModifier(StringModifier modifier) {
+		this.modifier.remove(modifier);
+	}
+	
+	void updatedStringModifier(StringModifier modifier, Map<String, Object> properties) {
+		System.out.println(modifier.getClass() + " configuration updated");
+		properties.forEach((key, value) -> System.out.println(key + " = " + value));
 	}
 
 	public void modify(String input) {
-		System.out.println(modifier.modify(input));
+		modifier.forEach(m -> System.out.println(m.modify(input)));
 	}
 }
